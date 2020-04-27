0d8a2a6188823d2d9a59b29f0bbdeea16535fe50
james.hume@utonomy.co.uk <james.hume@utonomy.co.uk>
2020-04-27 11:36:12 +0100

    Update to get EPUBs and PDFs and be able to restart if halted half way through

--- get_freebooks.py
+++ get_freebooks.py
@@ -28,15 +28,21 @@ for page_no, df in enumerate(df_list):
 			for link in soup.findAll('a', attrs={'title': re.compile("^Download this book")}):
 				links.add(link.get('href'))
 			
-			if len(links) != 0:
-				if len(links) > 1: print('### WARNING TOO MANY LINKS. CHECK THIS.')
-				pdf_url = 'https://link.springer.com{}'.format(links.pop())
-				print("   - Downloading from", pdf_url)
-				pdf = requests.get(pdf_url)
-				with open('springerbooks/{}.pdf'.format(row[1].replace(" ", "_")), 'wb') as f:
-					f.write(pdf.content)
+			if len(links) > 0:
+				for relative_url in links:
+					filename = relative_url.split("/")[-1]
+					file_ext = filename.split(".")[-1]
+					# Use page_no and index to make titles unique in case same title by different authors.
+					dest_file = 'springerbooks/{}_{}_{}.{}'.format(row[1].replace(" ", "_"), page_no, index, file_ext)
+					pdf_url = 'https://link.springer.com{}'.format(relative_url)
+					if not os.path.exists(dest_file):
+						print("   - Downloading from", pdf_url, "to", dest_file)
+						pdf = requests.get(pdf_url)
+						with open(dest_file, 'wb') as f:
+							f.write(pdf.content)
+					else:
+						print("   - The URL ", pdf_url, "already downloaded to", dest_file)
 			else:
 				print("### FAILED TO PARSE LINK FOR {}:{}".format(row[0], row[1]))
 		except Exception as e:
 			print("### FAILED for ", row[1], e)
-			sys.exit(1)
